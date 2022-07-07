1acd277dad1e51b635b79ab1eacf7b746e9888f2
tungnq <tungnq@unicloud.com.vn>
2022-07-07 16:12:44 +0700

    Directive

---
+++ src/app/directive/HighLight.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HighLightDirective } from './HighLight.directive';
+
+describe('Directive: HighLight', () => {
+  it('should create an instance', () => {
+    const directive = new HighLightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
