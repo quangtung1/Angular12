c9a6563a732ed44d3f4c74b4bd8d5fb4444f7884
tungnq <tungnq@unicloud.com.vn>
2022-07-07 14:35:45 +0700

    CascadingCombox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,15 +11,34 @@ export class HomeComponent implements OnInit {
   public age = 25;
   public traiCay=['Táo','Vải','Cam','Quýt'];
   public traiCay2 = [{ten:'Táo',gia:120000},{ten:'Vải',gia:0}];
+  public city = [{city:'An Giang',district:['Thành phố Châu Đốc','Thành phố Long Xuyên','Huyện Phú Châu','Huyện Châu Thành']}
+  ,{city:'Bắc Kạn',district:['Thành phố Bắc Kạn','Huyện Na Rì','Huyện Chợ Đồn','Huyện Ba Bể']} ];
+public districts:string[]=[];
+
   constructor() { }
 
   ngOnInit(): void {
-    console.log('Trái cây = ',this.traiCay);
+    console.log('Trái cây = ',this.city);
 
   }
   public resetName():void{
     console.log('resetName');
     this.name = '';
+  }
+  public changeCity(event: any){
+    console.log('event',event.target.value);
+    const city = event.target.value;
+    //Cách 1
+    // const search = this.city.filter(data=>data.city === city);
+    // console.log('Search: '+ search);
+
+    // if(search && search.length>0){
+    //   this.districts = search[0].district;
+    // }
+    //Cách 2
+
+    this.districts = this.city.find(data => data.city==city)?.district||[];
+
   }
 
 }
