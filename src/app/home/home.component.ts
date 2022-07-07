7de464445b1c4d4fabd38d97f2fed994178cb9cd
tungnq <tungnq@unicloud.com.vn>
2022-07-07 10:39:09 +0700

    DataBindingProperties and ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public name = 'Tùng Đẹp Trai';
-
+  public age = 25;
   constructor() { }
 
   ngOnInit(): void {
