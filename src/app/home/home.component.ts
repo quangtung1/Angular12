f7d84abdd69243f4995a5ccc13361cd02e1ab2ab
tungnq <tungnq@unicloud.com.vn>
2022-07-07 13:15:07 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,12 @@ export class HomeComponent implements OnInit {
 
   public name = 'Tùng Đẹp Trai';
   public age = 25;
+  public traiCay=['Táo','Vải','Cam','Quýt'];
+  public traiCay2 = [{ten:'Táo',gia:120000},{ten:'Vải',gia:0}];
   constructor() { }
 
   ngOnInit(): void {
-
+    console.log('Trái cây = ',this.traiCay);
 
   }
   public resetName():void{
